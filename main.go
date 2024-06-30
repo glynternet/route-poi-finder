c314b7a9048fdc7e1a78b9da8d31e25617e4683e
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2024-06-29 22:39:17 -0600

    more symbols, configurable radius

--- main.go
+++ main.go
@@ -33,144 +33,204 @@ const (
 	ExistsNo
 )
 
-type query []struct {
-	tag    string
-	values []string
-	not    string
-	exists int
+type query struct {
+	radius     int
+	conditions []condition
 }
 
-var queries = []query{{{
-	// - - amenity~"^(bar|biergarten|cafe|fast_food|food_court|fuel|ice_cream|pub|restaurant|bicycle_repair_station|compressed_air|drinking_water|shelter|toilets|water_point|marketplace|place_of_worship)$"
-	tag: "amenity",
-	values: []string{
-		"bar",
-		"bicycle_rental",
-		"bicycle_repair_station",
-		"bicycle_wash",
-		"biergarten",
-		"cafe",
-		"compressed_air",
-		"drinking_water",
-		"fast_food",
-		"food_court",
-		"fountain",
-		"fuel",
-		"ice_cream",
-		"marketplace",
-		"place_of_worship",
-		"pub",
-		"public_bath",
-		"restaurant",
-		"shelter",
-		"shower",
-		"toilets",
-		"water_point",
-		"watering_place",
-	},
-}}, {{
-	// - - tourism~"^(alpine_hut|camp_pitch|camp_site|guest_house|hostel|picnic_site|viewpoint|wilderness_hut)$"
-	tag: "tourism",
-	values: []string{
-		"alpine_hut",
-		"camp_pitch",
-		"camp_site",
-		"guest_house",
-		"hostel",
-		"picnic_site",
-		"viewpoint",
-		"wilderness_hut",
-	},
-}}, {{
-	// - - leisure~"^(nature_reserve|park|picnic_table|wildlife_hide)$"
-	tag: "leisure",
-	values: []string{
-		"nature_reserve",
-		"park",
-		"picnic_table",
-		"wildlife_hide",
-	},
-}}, {{
-	// - - natural~"^(spring|peak)$"
-	tag: "natural",
-	values: []string{
-		"spring",
-		"peak",
-		"mountain_range",
-		"ridge",
-		"arete",
-		"hot spring",
-		"plateu",
-		"saddle",
-	},
-}}, {{
-	// - - man_made~"^(spring_box|water_well|water_tap)$"
-	tag: "man_made",
-	values: []string{
-		"spring_box",
-		"water_well",
-		"water_tap",
-		"drinking_fountain",
-	},
-}}, {{
-	tag: "drinking_water",
-	values: []string{
-		"yes",
-	},
-}}, {{
-	tag:    "waterway",
-	exists: ExistsYes,
-}}, {{
-	// - - place~"^(town|village|hamlet|city|neighbourhood)$"
-	tag: "place",
-	values: []string{
-		"town",
-		"village",
-		"hamlet",
-		"city",
-		"neighbourhood",
-	},
-}}, {
+type condition struct {
+	tag       string
+	distance  int
+	values    []string
+	notValues []string
+	exists    int
+}
+
+var queries = []query{{
+	conditions: []condition{{
+		tag: "amenity",
+		values: []string{
+			"bar",
+			"bicycle_rental",
+			"bicycle_repair_station",
+			"bicycle_wash",
+			"biergarten",
+			"cafe",
+			"compressed_air",
+			"fast_food",
+			"food_court",
+			"fountain",
+			"fuel",
+			"ice_cream",
+			"marketplace",
+			"place_of_worship",
+			"pub",
+			"public_bath",
+			"restaurant",
+			"shelter",
+			"shower",
+			"toilets",
+		},
+	}},
+}, {
+	radius: 200,
+	conditions: []condition{{
+		tag: "amenity",
+		values: []string{
+			"drinking_water",
+			"water_point",
+			"watering_place",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		// - - tourism~"^(alpine_hut|camp_pitch|camp_site|guest_house|hostel|picnic_site|viewpoint|wilderness_hut)$"
+		tag: "tourism",
+		values: []string{
+			"alpine_hut",
+			"camp_pitch",
+			"camp_site",
+			"guest_house",
+			"hostel",
+			"picnic_site",
+			"viewpoint",
+			"wilderness_hut",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		// - - leisure~"^(nature_reserve|park|picnic_table|wildlife_hide)$"
+		tag: "leisure",
+		values: []string{
+			"nature_reserve",
+			"park",
+			"picnic_table",
+			"wildlife_hide",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		// - - natural~"^(spring|peak)$"
+		tag: "natural",
+		values: []string{
+			"spring",
+			"peak",
+			"mountain_range",
+			"ridge",
+			"arete",
+			"hot spring",
+			"plateu",
+			"saddle",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		// boundary=aboriginal_lands
+		//boundary=national_park
+		//boundary=forest
+		//boundary=water_protection_area
+		//boundary=protected_area
+		tag: "boundary",
+		values: []string{
+			"protected_area",
+			"aboriginal_lands",
+			"national_park",
+			"forest",
+			"water_protection_area",
+		},
+	}},
+}, {
+	radius: 200,
+	conditions: []condition{{
+		// - - man_made~"^(spring_box|water_well|water_tap)$"
+		tag: "man_made",
+		values: []string{
+			"spring_box",
+			"water_well",
+			"water_tap",
+			"drinking_fountain",
+		},
+	}},
+}, {
+	radius: 200,
+	conditions: []condition{{
+		tag: "drinking_water",
+		values: []string{
+			"yes",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		tag:    "waterway",
+		exists: ExistsYes,
+	}, {
+		tag: "waterway",
+		notValues: []string{
+			"drain",
+			"dam",
+			"stream", // may be good but is too high frequency to deal with atm
+			"ditch",
+			"canal",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		// - - place~"^(town|village|hamlet|city|neighbourhood)$"
+		tag: "place",
+		values: []string{
+			"town",
+			"village",
+			"hamlet",
+			"city",
+			"neighbourhood",
+		},
+	}},
+}, {
+	radius: 200,
 	//- - amenity="fountain"
 	//  - drinking_water!="no"
 	//  - drinking_water~".+"
-	{
+	conditions: []condition{{
 		tag:    "amenity",
 		values: []string{"fountain"},
 	}, {
 		tag:    "drinking_water",
-		values: []string{".+"},
+		exists: ExistsYes,
 	}, {
-		tag: "drinking_water",
-		not: "no",
-	},
-}, {{
-	tag: "shop",
-	values: []string{
-		"bakery",
-		"cheese",
-		"coffee",
-		"convenience",
-		"dairy",
-		"farm",
-		"food",
-		"greengrocer",
-		"health_food",
-		"ice_cream",
-		"pastry",
-		"tortilla",
-		"water",
-		"general",
-		"kiosk",
-		"supermarket",
-		"chemist",
-		"bicycle",
-		"sports",
-	},
-}}, {{
-	tag:    "mountain_pass",
-	values: []string{"yes"},
-}}}
+		tag:       "drinking_water",
+		notValues: []string{"no"},
+	}},
+}, {
+	conditions: []condition{{
+		tag: "shop",
+		values: []string{
+			"bakery",
+			"cheese",
+			"coffee",
+			"convenience",
+			"dairy",
+			"farm",
+			"food",
+			"greengrocer",
+			"health_food",
+			"ice_cream",
+			"pastry",
+			"tortilla",
+			"water",
+			"general",
+			"kiosk",
+			"supermarket",
+			"chemist",
+			"bicycle",
+			"sports",
+		},
+	}},
+}, {
+	conditions: []condition{{
+		tag:    "mountain_pass",
+		values: []string{"yes"},
+	}}},
+}
 
 // API
 
@@ -259,21 +319,28 @@ func mainErr() error {
 	}
 	log.Println("splits:", len(splits), "points:", splitPoints)
 
+	getPoint, getStats := point()
 	var pois []Point
 	for _, split := range splits {
-		aroundRoute, err := queryRouteComponent(80, split)
-		if err != nil {
-			return fmt.Errorf("creating query route component: %w", err)
-		}
+		for _, query := range queries {
+			locus := 80
+			// check not negative, could also memoize
+			if query.radius != 0 {
+				locus = query.radius
+			}
+			aroundRoute, err := queryRouteComponent(locus, split)
+			if err != nil {
+				return fmt.Errorf("creating query route component: %w", err)
+			}
 
-		for _, elements := range queries {
-			nodes, err := nodes(elements, aroundRoute)
+			nodes, err := nodes(query, aroundRoute)
 			if err != nil {
 				return fmt.Errorf("getting nodes: %w", err)
 			}
+			log.Println("Retrieved nodes:", len(nodes))
 
 			for _, node := range nodes {
-				pt, err := point(node.Tags, LatLon{
+				pt, err := getPoint(node.Tags, LatLon{
 					Lat: node.Lat,
 					Lon: node.Lon,
 				})
@@ -282,12 +349,14 @@ func mainErr() error {
 				}
 				pois = append(pois, pt)
 			}
-			wayCentres, err := wayCentres(elements, aroundRoute)
+			wayCentres, err := wayCentres(query.conditions, aroundRoute)
 			if err != nil {
 				return fmt.Errorf("getting way centres: %w", err)
 			}
+			log.Println("Retrieved way centres:", len(wayCentres))
+
 			for _, wayCentre := range wayCentres {
-				pt, err := point(wayCentre.Tags, wayCentre.Centre)
+				pt, err := getPoint(wayCentre.Tags, wayCentre.Centre)
 				if err != nil {
 					return fmt.Errorf("getting point for way(%v): %w", wayCentre, err)
 				}
@@ -326,27 +395,89 @@ func mainErr() error {
 	if err := f.Close(); err != nil {
 		return fmt.Errorf("closing output json file(%s): %w", f.Name(), err)
 	}
-	log.Println("output:", f.Name(), "points:", len(pois))
+	const topK = 50
+	stats := getStats(topK)
+	log.Println("top", topK, "tags")
+	for _, tagOccurrence := range stats.tagOccurrences {
+		log.Println("-", tagOccurrence.freq, tagOccurrence.value)
+	}
+	log.Println("top", topK, "tag values")
+	for _, tagValueOccurrence := range stats.tagValueOccurrences {
+		log.Println("-", tagValueOccurrence.freq, tagValueOccurrence.value)
+	}
+
+	log.Println("output:", f.Name(), "pois:", len(pois))
 	return nil
 }
 
-func point(tags map[string]interface{}, latLon LatLon) (Point, error) {
-	name, err := resolveName(tags)
-	if err != nil {
-		return Point{}, fmt.Errorf("resolving name from tags(%v): %w", tags, err)
-	}
-	desc, err := json.Marshal(tags)
-	if err != nil {
-		return Point{}, fmt.Errorf("marshalling node tags for description: %w", err)
-	}
-	nodePoint := Point{
-		Name:        name,
-		Lat:         latLon.Lat,
-		Lon:         latLon.Lon,
-		Description: string(desc),
-		Symbol:      resolveSymbol(tags),
-	}
-	return nodePoint, nil
+type stats struct {
+	tagOccurrences      []valueFreq
+	tagValueOccurrences []valueFreq
+}
+
+type valueFreq struct {
+	value string
+	freq  int
+}
+
+func point() (func(tags map[string]interface{}, latLon LatLon) (Point, error), func(topK int) stats) {
+	tagOccurrences := make(map[string]int)
+	tagValueOccurrences := make(map[string]int)
+	return func(tags map[string]interface{}, latLon LatLon) (Point, error) {
+			name, err := resolveName(tags)
+			if err != nil {
+				return Point{}, fmt.Errorf("resolving name from tags(%v): %w", tags, err)
+			}
+			desc, err := json.Marshal(tags)
+			if err != nil {
+				return Point{}, fmt.Errorf("marshalling node tags for description: %w", err)
+			}
+			for tag, value := range tags {
+				tagOccurrences[tag]++
+				tagValueOccurrences[tag+":"+value.(string)]++
+			}
+			nodePoint := Point{
+				Name:        name,
+				Lat:         latLon.Lat,
+				Lon:         latLon.Lon,
+				Description: string(desc),
+				Symbol:      resolveSymbol(tags),
+			}
+			return nodePoint, nil
+		}, func(topK int) stats {
+			var outputTagOccurrences []valueFreq
+			for tag, freq := range tagOccurrences {
+				outputTagOccurrences = append(outputTagOccurrences, valueFreq{
+					value: tag,
+					freq:  freq,
+				})
+			}
+			sort.Slice(outputTagOccurrences, func(i, j int) bool {
+				// descending
+				return outputTagOccurrences[i].freq > outputTagOccurrences[j].freq
+			})
+			if len(outputTagOccurrences) > topK {
+				outputTagOccurrences = outputTagOccurrences[:topK]
+			}
+			var outputTagValueOccurrences []valueFreq
+			for tagValue, freq := range tagValueOccurrences {
+				outputTagValueOccurrences = append(outputTagValueOccurrences, valueFreq{
+					value: tagValue,
+					freq:  freq,
+				})
+			}
+			sort.Slice(outputTagValueOccurrences, func(i, j int) bool {
+				return outputTagValueOccurrences[i].freq > outputTagValueOccurrences[j].freq
+			})
+			// descending
+			if len(outputTagValueOccurrences) > topK {
+				outputTagValueOccurrences = outputTagValueOccurrences[:topK]
+			}
+			return stats{
+				tagOccurrences:      outputTagOccurrences,
+				tagValueOccurrences: outputTagValueOccurrences,
+			}
+		}
 }
 
 func queryRouteComponent(locus int, route []gpxgo.GPXPoint) (string, error) {
@@ -389,7 +520,7 @@ out meta;`); err != nil {
 }
 
 func nodes(elements query, aroundRoute string) ([]element, error) {
-	responseElements, err := queryResponseElements(`node`, elements, aroundRoute)
+	responseElements, err := queryResponseElements(`node`, elements.conditions, aroundRoute)
 	if err != nil {
 		return nil, fmt.Errorf("getting query response elements: %w", err)
 	}
@@ -403,8 +534,8 @@ func nodes(elements query, aroundRoute string) ([]element, error) {
 	return responseElements, nil
 }
 
-func wayCentres(elements query, route string) ([]wayCentre, error) {
-	responseElements, err := queryResponseElements(`way`, elements, route)
+func wayCentres(conditions []condition, route string) ([]wayCentre, error) {
+	responseElements, err := queryResponseElements(`way`, conditions, route)
 	if err != nil {
 		return nil, fmt.Errorf("getting query response elements: %w", err)
 	}
@@ -446,13 +577,13 @@ func wayCentres(elements query, route string) ([]wayCentre, error) {
 	return wayCentres, nil
 }
 
-func queryResponseElements(queryType string, query query, route string) ([]element, error) {
+func queryResponseElements(queryType string, queryConditions []condition, route string) ([]element, error) {
 	var sb strings.Builder
 	sb.WriteString(`[out:json];` + queryType)
-	for _, element := range query {
+	for _, element := range queryConditions {
 		var definedConditions int
 		for _, condition := range []bool{
-			element.not != "",
+			len(element.notValues) > 0,
 			len(element.values) > 0,
 			element.exists != ExistsUndefined,
 		} {
@@ -463,26 +594,30 @@ func queryResponseElements(queryType string, query query, route string) ([]eleme
 		if definedConditions > 1 {
 			return nil, fmt.Errorf("query element must contain only one condition: 'not', 'values' or 'exists': %+v", element)
 		}
-		var elementCondition string
+		var elementConditions []string
 		switch {
 		case len(element.values) > 0:
-			elementCondition = fmt.Sprintf(`%s~"^(%s)$"`, element.tag, strings.Join(element.values, "|"))
-		case element.not != "":
-			elementCondition = fmt.Sprintf(`%s!="%s"`, element.tag, element.not)
+			elementConditions = []string{fmt.Sprintf(`%s~"^(%s)$"`, element.tag, strings.Join(element.values, "|"))}
+		case len(element.notValues) > 0:
+			for _, notValue := range element.notValues {
+				elementConditions = append(elementConditions, fmt.Sprintf(`%s!="%s"`, element.tag, notValue))
+			}
 		case element.exists != ExistsUndefined:
 			switch element.exists {
 			case ExistsYes:
-				elementCondition = fmt.Sprintf(`%s`, element.tag)
+				elementConditions = []string{fmt.Sprintf(`%s`, element.tag)}
 			case ExistsNo:
-				elementCondition = fmt.Sprintf(`!%s`, element.tag)
+				elementConditions = []string{fmt.Sprintf(`!%s`, element.tag)}
 			default:
 				return nil, fmt.Errorf("unsupported exists value: %+v", element.exists)
 			}
 		default:
 			return nil, fmt.Errorf("query element contains no conditions: %+v", element)
 		}
-		if _, err := sb.WriteString(`[` + elementCondition + `]`); err != nil {
-			return nil, fmt.Errorf("writing query element: %w", err)
+		for _, elementCondition := range elementConditions {
+			if _, err := sb.WriteString(`[` + elementCondition + `]`); err != nil {
+				return nil, fmt.Errorf("writing query element: %w", err)
+			}
 		}
 	}
 	sb.WriteString(route)
@@ -500,20 +635,20 @@ func queryResponseElements(queryType string, query query, route string) ([]eleme
 		log.Printf("query fetched from cached result: %s", queryStateFilePath)
 		rc = stored
 	} else if os.IsNotExist(err) {
-		log.Printf("query result not cached, making query to API: %s:%+v", queryType, query)
+		log.Printf("query result not cached, making query to API: %s:%+v", queryType, queryConditions)
 		// curl -d @<(cat <(echo "[out:json];$type$params") ~/tmp/pois/query_end) -X POST http://overpass-api.de/api/interpreter
 		resp, err := http.Post(`http://overpass-api.de/api/interpreter`, "", strings.NewReader(renderedQuery))
 		if err != nil {
-			return nil, fmt.Errorf("posting query(%+v): %w", query, err)
+			return nil, fmt.Errorf("posting query(%+v): %w", queryConditions, err)
 		}
 		if resp.StatusCode != http.StatusOK {
 			_, _ = io.Copy(os.Stderr, resp.Body)
 			_ = resp.Body.Close()
-			return nil, fmt.Errorf("posting query (%s:%s): unexpected status code %d (%s)", queryType, query, resp.StatusCode, resp.Status)
+			return nil, fmt.Errorf("posting query (%s:%s): unexpected status code %d (%s)", queryType, queryConditions, resp.StatusCode, resp.Status)
 		}
 		file, err := os.OpenFile(queryStateFilePath, os.O_RDWR|os.O_CREATE, 0666)
 		if err != nil {
-			return nil, fmt.Errorf("opening query file for writing(%+v): %w", query, err)
+			return nil, fmt.Errorf("opening query file for writing(%+v): %w", queryConditions, err)
 		}
 		if _, err := io.Copy(file, resp.Body); err != nil {
 			return nil, fmt.Errorf("outputing response body: %w", err)
@@ -550,6 +685,8 @@ func resolveName(tags map[string]interface{}) (string, error) {
 		"shop",
 		"waterway",
 		"natural",
+		// probably good to combine this with another tag
+		"boundary",
 	} {
 		n, ok := tags[tag].(string)
 		if ok {
@@ -565,7 +702,9 @@ func resolveSymbol(tags map[string]interface{}) string {
 		tags   map[string]string
 		symbol string
 	}{
+		{tags: map[string]string{"shop": "convenience"}, symbol: "Shopping Center"},
 		{tags: map[string]string{"leisure": "park"}, symbol: "Park"},
+		{tags: map[string]string{"boundary": "protected_area"}, symbol: "Park"},
 		{tags: map[string]string{"amenity": "toilets"}, symbol: "Restroom"},
 		{tags: map[string]string{"amenity": "drinking_water"}, symbol: "Drinking Water"},
 		{tags: map[string]string{"natural": "peak"}, symbol: "Summit"},
@@ -574,15 +713,22 @@ func resolveSymbol(tags map[string]interface{}) string {
 		{tags: map[string]string{"amenity": "fast_food"}, symbol: "Fast Food"},
 		{tags: map[string]string{"amenity": "fuel"}, symbol: "Gas Station"},
 		{tags: map[string]string{"amenity": "pub"}, symbol: "Bar"},
+		{tags: map[string]string{"amenity": "bar"}, symbol: "Bar"},
 		{tags: map[string]string{"amenity": "cafe"}, symbol: "Restaurant"},
 		{tags: map[string]string{"tourism": "picnic_site"}, symbol: "Picnic Area"},
 		{tags: map[string]string{"amenity": "restaurant", "cuisine": "pizza"}, symbol: "Pizza"},
 		{tags: map[string]string{"amenity": "restaurant"}, symbol: "Restaurant"},
 		{tags: map[string]string{"amenity": "ice_cream"}, symbol: "Fast Food"},
 		{tags: map[string]string{"tourism": "camp_pitch"}, symbol: "Campground"},
+		{tags: map[string]string{"tourism": "camp_site"}, symbol: "Campground"},
 		{tags: map[string]string{"leisure": "nature_reserve"}, symbol: "Park"},
 		{tags: map[string]string{"amenity": "shelter"}, symbol: "Building"},
 		{tags: map[string]string{"amenity": "place_of_worship"}, symbol: "Church"},
+		{tags: map[string]string{"place": "town"}, symbol: "City Hall"},
+		{tags: map[string]string{"place": "village"}, symbol: "City Hall"},
+		{tags: map[string]string{"place": "hamlet"}, symbol: "City Hall"},
+		{tags: map[string]string{"place": "city"}, symbol: "City Hall"},
+		{tags: map[string]string{"place": "neighbourhood"}, symbol: "City Hall"},
 	} {
 		match := true
 		for k, matcherV := range symbolMatchers.tags {
